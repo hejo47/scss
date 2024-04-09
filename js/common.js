e6b2b99ebb43f76d4d91555b7328a8e6dc18ca56
hejo47 <hejo_47@naver.com>
2024-04-09 09:58:36 +0900

    feat: marquee 적용 중

--- js/common.js
+++ js/common.js
@@ -7,7 +7,6 @@ window.onload = function(){
       const modalID = this.dataset.modal;
       document.querySelector(modalID).classList.add("active");
       scrollForbid(modalID);
-      console.log(1);
     });
   });
 
@@ -17,6 +16,11 @@ window.onload = function(){
       modalClose(elem);
     });
   });
+
+  // MARQUEE
+  const marqueeWrapWidth = document.querySelector(".marquee__wrap").offsetWidth;
+  const marquee = document.querySelector(".marquee");
+  marquee.style.setProperty('--MARQUEE-SPANWIDTH', marqueeWrapWidth + 15 + "px");
 }
 
 function modalClose(close){
@@ -57,61 +61,62 @@ addClassList.forEach(function(addClassElem, idx) {
 
 // SELECT
 const selectList = document.querySelectorAll(".select");
-  selectList.forEach(function(select){
-    select.addEventListener("click", function(){ // 누름
-      if(select.classList.contains("active")) { // 그럼 켜져야지 근데
-        select.classList.remove("active"); // 이미 켜져 있으면 끄고
-      } else if(select.classList.contains("active") == false) { // 꺼져 있으면
-        select.classList.add("active"); // 켜라
-        // 근데 눌렀을 때 다른 셀렉트가 이미 켜져있으면 그거 꺼야지 켜기 전에
-        const activeSelectList = document.querySelectorAll(".select");
-        // 셀렉트 중에 이미 활성화 된 게 있으면(방금 누른 건 아니어야 함)
-        activeSelectList.forEach(function(activeSelect){
-          if(activeSelect.classList.contains("active") && activeSelect !== select){
-            // 그거 먼저 꺼야지
-            activeSelect.classList.remove("active");
-          }
-        });
-      }
-    });
-    
-    // 셀렉트박스 값 넣기
-    const optionList = select.querySelectorAll(".select__item");
-    optionList.forEach(function(option){
-      let optionValue = option.dataset.value;
-      let selectID = option.closest(".select").dataset.id;
-      let valueSection = select.querySelector(".select__value");
-      option.addEventListener("click", function(){ // 옵션 누름
-        // console.log(selectID);
-        const seletedOptionList = select.querySelectorAll(".select__item");
-        valueSection.innerText = option.innerText; // 값 공간에 선택된 텍스트 넣기
-        // formSubmitHandler(select, optionValue);
-        seletedOptionList.forEach(function(seletedOption){ // 다른 옵션이
-          // 이미 선택되어 있으면
-          if(seletedOption.classList.contains("selected") && seletedOption !== option){
-            seletedOption.classList.remove("selected"); // 그거 해제해
-          } else {
-            return;
-          }
-        });
-        option.classList.add("selected"); // 삭제했으니까 선택함
-      });
-    });
-      
-  });
-
-  // function formSubmitHandler(elem, select) {
-  //   elem.querySelector("input").value = select;
-  //   elem.closest("form").submit();
-  // }
-
-  // 셀렉트박스 이외의 곳을 선택하면 셀렉트박스가 닫힘
-  document.addEventListener("click", function(e){
-    const target = e.target;
-    if(target.closest(".select") <= 0) {
+selectList.forEach(function(select){
+  select.addEventListener("click", function(){ // 누름
+    if(select.classList.contains("active")) { // 그럼 켜져야지 근데
+      select.classList.remove("active"); // 이미 켜져 있으면 끄고
+    } else if(select.classList.contains("active") == false) { // 꺼져 있으면
+      select.classList.add("active"); // 켜라
+      // 근데 눌렀을 때 다른 셀렉트가 이미 켜져있으면 그거 꺼야지 켜기 전에
       const activeSelectList = document.querySelectorAll(".select");
+      // 셀렉트 중에 이미 활성화 된 게 있으면(방금 누른 건 아니어야 함)
       activeSelectList.forEach(function(activeSelect){
-        activeSelect.classList.remove("active");
+        if(activeSelect.classList.contains("active") && activeSelect !== select){
+          // 그거 먼저 꺼야지
+          activeSelect.classList.remove("active");
+        }
       });
-    } 
+    }
   });
+  
+  // 셀렉트박스 값 넣기
+  const optionList = select.querySelectorAll(".select__item");
+  optionList.forEach(function(option){
+    let optionValue = option.dataset.value;
+    let selectID = option.closest(".select").dataset.id;
+    let valueSection = select.querySelector(".select__value");
+    option.addEventListener("click", function(){ // 옵션 누름
+      // console.log(selectID);
+      const seletedOptionList = select.querySelectorAll(".select__item");
+      valueSection.innerText = option.innerText; // 값 공간에 선택된 텍스트 넣기
+      // formSubmitHandler(select, optionValue);
+      seletedOptionList.forEach(function(seletedOption){ // 다른 옵션이
+        // 이미 선택되어 있으면
+        if(seletedOption.classList.contains("selected") && seletedOption !== option){
+          seletedOption.classList.remove("selected"); // 그거 해제해
+        } else {
+          return;
+        }
+      });
+      option.classList.add("selected"); // 삭제했으니까 선택함
+    });
+  });
+    
+});
+
+// function formSubmitHandler(elem, select) {
+//   elem.querySelector("input").value = select;
+//   elem.closest("form").submit();
+// }
+
+// 셀렉트박스 이외의 곳을 선택하면 셀렉트박스가 닫힘
+document.addEventListener("click", function(e){
+  const target = e.target;
+  if(target.closest(".select") <= 0) {
+    const activeSelectList = document.querySelectorAll(".select");
+    activeSelectList.forEach(function(activeSelect){
+      activeSelect.classList.remove("active");
+    });
+  } 
+});
+
